a4c82888d659b96438c29855c57934f031cfcd54
Arda Basoglu <ardabasoglu@gmail.com>
2025-01-03 21:20:22 +0300

    Update 'Get Started' link to redirect to login page; remove 'Learn More' link for a cleaner UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,15 +14,9 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row mt-8">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-8 sm:px-10"
-            href="#"
+            href="/login"
           >
             Get Started
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-8 sm:px-10"
-            href="#"
-          >
-            Learn More
           </a>
         </div>
       </main>
