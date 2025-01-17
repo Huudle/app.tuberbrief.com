009568b07294794cf6549a25bda2328ee9107d56
Arda Basoglu <ardabasoglu@gmail.com>
2025-01-17 11:57:36 +0300

    Refactor image domain configuration in Next.js setup
    - Replaced the static 'domains' array with 'remotePatterns' for image optimization.
    - Updated the configuration to allow wildcard subdomains for ytimg.com, enhancing flexibility in image sourcing.

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: [
-      "i.ytimg.com",
-      "yt3.ggpht.com",
-      "yt3.googleusercontent.com",
-      "ui-avatars.com",
-      "i4.ytimg.com",
-      "i3.ytimg.com",
-      "i2.ytimg.com",
-      "i1.ytimg.com",
-      "i0.ytimg.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.ytimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "yt3.ggpht.com",
+      },
+      {
+        protocol: "https",
+        hostname: "yt3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+      },
     ],
   },
 };
