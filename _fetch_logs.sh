7d22f5ffe9f3ff9e17320178ecb6ae3249b7e9fd
Arda Basoglu <ardabasoglu@gmail.com>
2025-02-11 19:29:25 +0300

    Add log fetching script for remote server
    - Create bash script to copy daily log files from remote server
    - Dynamically generate log file path based on current date
    - Use SCP to retrieve logs to local Downloads directory

---
+++ _fetch_logs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get the current date in YYYY/MM/DD format
+date_part=$(date +"%Y/%m/%d")
+
+# Define remote and local paths
+REMOTE_USER="ubuntu"
+REMOTE_HOST="141.144.244.28"
+REMOTE_PATH="/var/www/flow-fusion/logs/$date_part.log"
+LOCAL_PATH="$HOME/Downloads/"
+
+# Print the command that will be executed
+echo "Copying from: $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
+echo "Copying to: $LOCAL_PATH"
+
+# Run SCP command
+scp "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH" "$LOCAL_PATH"
+
