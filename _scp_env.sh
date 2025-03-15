475107d409ab65bf9203c8176f1e9421caf87c9d
Arda Basoglu <ardabasoglu@gmail.com>
2025-03-15 20:07:23 +0200

    Add script to copy .env.local to server
    - Create _scp_env.sh script for secure copying of .env.local file to the server
    - Include server connection details such as username, IP address, and path

---
+++ _scp_env.sh
@@ -0,0 +1,12 @@
+# Copy .env.local to the server
+# server username: ubuntu
+# server ip: 141.144.244.28
+# server path: /var/www/app.tuberbrief.com
+
+scp /Users/ardabasoglu/Projects/huudle/app.tuberbrief.com/.env.local ubuntu@141.144.244.28:/var/www/app.tuberbrief.com/.env.local
+
+
+
+
+
+
